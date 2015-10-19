b4abf9b50733f713fe4eb780ed4726d8fa4619ab
Adamczyk Mikolaj <mikolaj.adamczyk@vml.com>
2015-10-19 16:12:43 +0200

    Created application bundle.

---
+++ src/Contest/ApplicationBundle/ContestApplicationBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Contest\ApplicationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Created by PhpStorm.
+ * User: mikolaj.adamczyk
+ * Date: 19.10.15
+ * Time: 16:08
+ */
+class ContestApplicationBundle extends Bundle
+{
+
+}
