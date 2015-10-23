d8c7446ba71ca3ad18231a475dc83114e4ddeabf
Adamczyk Mikolaj <mikolaj.adamczyk@vml.com>
2015-10-23 14:30:28 +0200

    Adding DoctrineBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,6 +10,7 @@ class AppKernel extends Kernel
         return array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Contest\ApplicationBundle\ContestApplicationBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
         );
     }
 
